27e4b7ccb49d32ffc41a15c335996db588b7b4ec
Ernest Irabor <irabeny89@gmail.com>
2022-07-24 11:11:43 +0100

    Add pass test for signAndGetToken function

---
+++ __tests__/rest/register/signAndGetToken.test.ts
@@ -0,0 +1,14 @@
+import signAndGetToken from "utils/rest/register/signAndGetToken";
+// mock environment variables
+jest.mock("config", () => ({ envVariables: { tokenSecret: "mock secret" } }));
+
+describe("signAndGetToken", () => {
+  it("signs an object payload and return a token", () => {
+    const payload = {
+      email: "mock@gmail.com",
+      role: "mocker",
+      username: "jester",
+    };
+    expect(typeof signAndGetToken(payload)).toBe("string");
+  });
+});
