9cefe499d698e8c7d2bf136a11a6385b859f6d5b
Ernest Irabor <irabeny89@gmail.com>
2022-07-26 09:43:35 +0100

    Set up auth control for pages

--- config.ts
+++ config.ts
@@ -6,11 +6,13 @@ export const SAYFORMETOKEN = "sayforme";
 
 export const envVariables = {
   dbCompass: process.env.DB_COMPASS!,
-  tokenSecret: process.env.ENCRYPT_SECRET!,
+  tokenSecret:
+    process.env.ENCRYPT_SECRET! || process.env.NEXT_PUBLIC_ENCRYPT_SECRET!,
 };
 
 export const appData = {
   appName: "SayforMe",
+  version: "1.0",
   intro: [
     "If you want to call someone but you don't want to forget especially because it's their birthday, love message or something, then let SayForMe do it for you. Be a member today and register or login if you're already a member.",
   ],
--- utils/components/Layout.tsx
+++ utils/components/Layout.tsx
@@ -1,20 +1,38 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import getPageName from "utils/getPageName";
+import { SAYFORMETOKEN } from "config";
+import { verify } from "jsonwebtoken";
+import { envVariables } from "config";
 
 type LayoutProps = { children: ReactNode };
 
 export default function Layout({ children }: LayoutProps) {
-  const pageName = getPageName(useRouter().pathname);
+  const router = useRouter(),
+    pageName = getPageName(router.asPath),
+    handleLogout = () => (
+      localStorage.removeItem(SAYFORMETOKEN), router.push("/")
+    );
+
+  useEffect(() => {
+    const isProtected = ["BOOKINGS", "USERS"].includes(pageName);
+    const token = localStorage.getItem(SAYFORMETOKEN);
+    isProtected &&
+      (!token
+        ? router.push("/")
+        : !verify(token, envVariables.tokenSecret) && router.push("/"));
+  }, [pageName]);
+
   return (
     <main>
       <Head>
         <title>{pageName}</title>
       </Head>
       <Header />
+      <button onClick={handleLogout}>Logout</button>
       {children}
       <Footer />
     </main>
