ebc43979d7401ff5b9dc331e0586854fd995053e
Ernest Irabor <irabeny89@gmail.com>
2022-07-27 06:55:48 +0100

    Add check before handling with graphql handler

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,13 +1,21 @@
+import { envVariables } from "config";
 import { NextApiRequest, NextApiResponse } from "next";
 import apolloServer from "utils/api/graphql/server";
 
-const startedServer = apolloServer.start()
+const startedServer = apolloServer.start();
 export default async function graphqlHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await startedServer
-  return await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
+  return !req.headers.authorization
+    ? res.status(403).json({ code: 403, message: "Forbidden.", type: "error" })
+    : !(await import("jsonwebtoken")).verify(
+        req.headers.authorization!.replace("Bearer ", ""),
+        envVariables.tokenSecret
+      )
+    ? res.status(403).json({ code: 403, message: "Forbidden.", type: "error" })
+    : (await startedServer,
+      await apolloServer.createHandler({ path: "/api/graphql" })(req, res));
 }
 
 export const config = { api: { bodyParser: false } };
