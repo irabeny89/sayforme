2e1dcd7d5b9abcc054aae8bfa7882a37d0e7207f
Ernest Irabor <irabeny89@gmail.com>
2022-07-28 15:14:55 +0100

    Rename file name of Booking to booking

--- pages/bookings/[[...id]].tsx
+++ pages/bookings/[[...id]].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Bookings from "utils/components/bookings/Bookings";
+import Bookings from "utils/components/bookings";
 import Booking from "utils/components/bookings/Booking";
 
 export default function BookingRoutes() {
