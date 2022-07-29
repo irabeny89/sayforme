51f2e87c8f52466d99e346b7eb9d3e04b3e8d136
Ernest Irabor <irabeny89@gmail.com>
2022-07-30 00:05:30 +0100

    Move files to parent directory

--- pages/users/[[...id]].tsx
+++ pages/users/[[...id]].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
-import Users from "utils/components/bookings/users";
-import User from "utils/components/bookings/users/User";
+import Users from "utils/components/users";
+import User from "utils/components/users/User";
 
 export default function UserRoutes() {
   const { query } = useRouter();
