69fa4a6d421e06b43768bf10a04767b542753bbf
Ernest Irabor <irabeny89@gmail.com>
2022-07-30 02:51:22 +0100

    Add mongodb uri from env

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -1,12 +1,12 @@
 import mongoose, { connect } from "mongoose";
 import { envVariables } from "config";
 
-const { dbAtlas, dbCompass } = envVariables;
+// const { dbAtlas, dbCompass } = envVariables;
 
 export default async function dbConnection() {
   if (mongoose.connections[0].readyState !== 1) {
     mongoose.set("debug", process.env.NODE_ENV === "development");
-    await connect(dbCompass || dbAtlas || process.env.MONGO_URI!);
+    await connect(process.env.MONGO_URI!);
   }
 }
 
