9386389710f67701c7509acdd1fc6a7858dc0de9
Ernest Irabor <irabeny89@gmail.com>
2022-07-30 04:09:43 +0100

    Fix Atlas db issue; attached db name to uri

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -1,15 +1,16 @@
 import mongoose, { connect } from "mongoose";
 import { envVariables } from "config";
 
-const { dbAtlasVercel, dbCompass } = envVariables;
+const { dbAtlasVercel, dbCompass } = envVariables,
+  dbUri = dbCompass || dbAtlasVercel;
 
 export default async function dbConnection() {
   if (mongoose.connections[0].readyState !== 1) {
     mongoose.set("debug", process.env.NODE_ENV === "development");
-    await connect(dbCompass || dbAtlasVercel);
+    await connect(dbUri);
   }
 }
 
 mongoose.connection.once("open", () =>
-  console.log("Database connected: %s", envVariables.dbCompass)
+  console.log("Database connected: %s", dbCompass || dbAtlasVercel)
 );
