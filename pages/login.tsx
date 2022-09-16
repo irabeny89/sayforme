207741b1687adcbf533d2707e27a96f6833f8182
Ernest Irabor <irabeny89@gmail.com>
2022-09-17 00:54:15 +0100

    Refactor and restyle login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,39 +4,32 @@ export default function Login() {
   const { errorMessage, isLoading, handleSubmit } = useAuthRequest();
 
   return (
-    <div>
-      <h2>Login</h2>
-      <p>Fill the form below</p>
-      <form onSubmit={handleSubmit("login")}>
-        <fieldset>
-          <legend>Credentials</legend>
-          <label>
-            Email:
-            <div>
-              <input
-                type="email"
-                name="email"
-                placeholder="sayforme@gmail.com"
-                required
-              />
-            </div>
-          </label>
-          <br />
-          <label>
-            Password:
-            <div>
-              <input
-                type="password"
-                name="password"
-                placeholder="don't share password"
-                required
-              />
-            </div>
-          </label>
-        </fieldset>
-        <br />
-        <button type="submit">{isLoading ? "waiting..." : "Submit"}</button>
-      </form>
+    <div className="min-h-screen mt-10 space-y-10">
+      <h2 className="text-xl underline">Login</h2>
+      <div className="space-y-5 sm:w-1/2 mx-auto">
+        <p>Fill the form below</p>
+        <form onSubmit={handleSubmit("login")} className="space-y-7">
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+            className="input input-sm w-full max-w-xs"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password 8 or more letters"
+            required
+            className="input input-sm w-full max-w-xs"
+          />
+          <div>
+            <button type="submit" className="btn btn-sm">
+              {isLoading ? "waiting..." : "Submit"}
+            </button>
+          </div>
+        </form>
+      </div>
       <br />
       <small>{errorMessage}</small>
     </div>
