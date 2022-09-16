b10c748ada95240aea23c91260c00cc1f4d50846
Ernest Irabor <irabeny89@gmail.com>
2022-09-17 00:20:48 +0100

    Refactor & restyle header

--- utils/components/layout/Header.tsx
+++ utils/components/layout/Header.tsx
@@ -3,6 +3,9 @@ import { appData } from "config";
 import { useReactiveVar } from "@apollo/client";
 import { tokenPayloadVar } from "utils/api/graphql/client/reactiveVariables";
 import usePageTitle from "utils/hooks/usePageTitle";
+import { MdMenu } from "react-icons/md";
+import { GiNothingToSay } from "react-icons/gi";
+import useToggle from "utils/hooks/useToggle";
 
 const { appName, pages, adminAccessPages, userPrivatePages, publicPages } =
   appData;
@@ -13,6 +16,10 @@ export default function Header() {
   const isUser = payload?.email;
   const currentPageTitle = usePageTitle();
   const commonClassName = "hover:bg-white cursor-pointer p-1";
+  const { toggleFlag, toggler } = useToggle();
+  const menuBtnClassName = `sm:hidden transition-all duration-100 ml-auto my-2 ${
+    toggleFlag ? "rotate-90" : ""
+  }`;
   const getTabClassName = (title: string) =>
     currentPageTitle === title.toUpperCase()
       ? commonClassName + " border-b-2 border-secondary"
@@ -20,28 +27,35 @@ export default function Header() {
   const confirmUserAccess = (title: string) =>
     (adminAccessPages.includes(title) && isAdmin) ||
     (userPrivatePages.includes(title) && isUser);
-    
+
   return (
-    <header className="p-3 flex justify-between">
-      <h1>
-        <Link href="/">{appName}</Link>
+    <header className="py-5 flex justify-between">
+      <h1 className="text-2xl font-bold">
+        <Link href="/">
+          <>
+            <GiNothingToSay /> {appName}
+          </>
+        </Link>
       </h1>
-      <nav>
-        <ul className="flex gap-2">
-          {pages.map(({ route, title }) =>
-            confirmUserAccess(title) ? (
-              <Link key={title} href={route}>
-                <li className={getTabClassName(title)}>{title}</li>
-              </Link>
-            ) : (
-              publicPages.includes(title) && (
+      <nav className="relative">
+        <MdMenu size={20} onClick={toggler} className={menuBtnClassName} />
+        {toggleFlag && (
+          <ul className="sm:flex absolute bg-base-300 sm:bg-inherit right-0 sm:static w-32 sm:w-auto space-y-3 sm:space-y-0 p-2 sm:p-0 gap-3">
+            {pages.map(({ route, title }) =>
+              confirmUserAccess(title) ? (
                 <Link key={title} href={route}>
                   <li className={getTabClassName(title)}>{title}</li>
                 </Link>
+              ) : (
+                publicPages.includes(title) && (
+                  <Link key={title} href={route}>
+                    <li className={getTabClassName(title)}>{title}</li>
+                  </Link>
+                )
               )
-            )
-          )}
-        </ul>
+            )}
+          </ul>
+        )}
       </nav>
     </header>
   );
