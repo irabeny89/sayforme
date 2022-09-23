e6ac5c2bffbeb45df63d04b33ce02646f59db473
Ernest Irabor <irabeny89@gmail.com>
2022-09-23 23:40:03 +0100

    Set correct html tag hierachy

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,11 @@ const Home: NextPage = () => {
   return (
     <div className="min-h-screen mt-10 space-y-10">
       <h2 className="text-xl underline">Introduction</h2>
-      <p className="space-y-5 sm:w-1/2 mx-auto">
+      <div className="space-y-5 sm:w-1/2 mx-auto">
         {intro.map((p, i) => (
-          <div key={i}>{p}</div>
+          <p key={i}>{p}</p>
         ))}
-      </p>
+      </div>
       <div className="flex gap-2">
         <div className="underline">
           <Link href="/register">Register</Link>
