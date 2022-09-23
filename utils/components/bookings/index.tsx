bd37c61db3fc1bddd6efa3279419852fe78a7d03
Ernest Irabor <irabeny89@gmail.com>
2022-09-24 00:38:21 +0100

    Restyle

--- utils/components/bookings/index.tsx
+++ utils/components/bookings/index.tsx
@@ -10,15 +10,25 @@ export default function Bookings() {
   return (
     <div className="min-h-screen mt-10 space-y-10">
       <h2 className="text-xl underline">Bookings</h2>
-      <div className="sm:w-1/2 mx-auto">
+      <div className="sm:w-1/2 md:w-fit mx-auto">
         <p>List of all Call Bookings</p>
         <dialog open={openModal} className="z-20">
-          <button onClick={handleCloseModal} className="float-right text-secondary">X</button>
+          <button
+            onClick={handleCloseModal}
+            className="float-right text-secondary"
+          >
+            X
+          </button>
           <h3>Booking</h3>
           <p>Fill the form below to create a booking:</p>
           <AddBookingForm handleCloseModal={handleCloseModal} />
         </dialog>
-        <button onClick={handleOpenModal} className="btn btn-sm border-0 bg-primary my-5">Add Booking</button>
+        <button
+          onClick={handleOpenModal}
+          className="btn btn-sm border-0 bg-primary my-5"
+        >
+          Add Booking
+        </button>
         <BookingsTable />
       </div>
     </div>
