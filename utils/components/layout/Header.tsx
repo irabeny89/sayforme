aa0ae203b2dcac6448c3e2e48edfe016ae4e5939
Ernest Irabor <irabeny89@gmail.com>
2022-09-24 00:11:10 +0100

    Refactor header nav tabs logic

--- utils/components/layout/Header.tsx
+++ utils/components/layout/Header.tsx
@@ -9,54 +9,64 @@ import useToggle from "utils/hooks/useToggle";
 
 const { appName, pages, adminAccessPages, userPrivatePages, publicPages } =
   appData;
+const commonClassName = "hover:bg-white cursor-pointer p-1";
 
-export default function Header() {
+function NavigationTabs() {
   const payload = useReactiveVar(tokenPayloadVar);
   const isAdmin = payload?.role === "ADMIN";
   const isUser = payload?.email;
   const currentPageTitle = usePageTitle();
-  const commonClassName = "hover:bg-white cursor-pointer p-1";
-  const { toggleFlag, toggler } = useToggle();
-  const menuBtnClassName = `sm:hidden transition-all duration-100 ml-auto my-2 ${
-    toggleFlag ? "rotate-90" : ""
-  }`;
+  const confirmUserAccess = (title: string) =>
+    (adminAccessPages.includes(title) && isAdmin) ||
+    (userPrivatePages.includes(title) && isUser);
   const getTabClassName = (title: string) =>
     currentPageTitle === title.toUpperCase()
       ? commonClassName + " border-b-2 border-secondary"
       : commonClassName;
-  const confirmUserAccess = (title: string) =>
-    (adminAccessPages.includes(title) && isAdmin) ||
-    (userPrivatePages.includes(title) && isUser);
+
+  return (
+    <nav className="relative">
+      <ul className="sm:flex absolute z-10 bg-base-300 sm:bg-inherit right-0 top-5 sm:static w-32 sm:w-auto space-y-3 sm:space-y-0 p-2 sm:p-0 gap-3">
+        {pages.map(({ route, title }) =>
+          confirmUserAccess(title) ? (
+            <Link key={title} href={route}>
+              <li className={getTabClassName(title)}>{title}</li>
+            </Link>
+          ) : (
+            publicPages.includes(title) && (
+              <Link key={title} href={route}>
+                <li className={getTabClassName(title)}>{title}</li>
+              </Link>
+            )
+          )
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default function Header() {
+  const { toggleFlag, toggler } = useToggle();
+  const menuBtnClassName = `sm:hidden transition-all duration-100 ml-auto my-2 ${
+    toggleFlag ? "rotate-90" : ""
+  }`;
 
   return (
     <header className="py-5 flex justify-between">
       <h1 className="text-2xl font-bold">
         <Link href="/">
-          <div className="cursor-pointer">
-            <GiNothingToSay /> {appName}
+          <div className="cursor-pointer -space-y-2">
+            <GiNothingToSay /> <div>{appName}</div>
           </div>
         </Link>
       </h1>
-      <nav className="relative">
+      <div className="sm:hidden">
         <MdMenu size={20} onClick={toggler} className={menuBtnClassName} />
-        {toggleFlag && (
-          <ul className="sm:flex absolute z-10 bg-base-300 sm:bg-inherit right-0 sm:static w-32 sm:w-auto space-y-3 sm:space-y-0 p-2 sm:p-0 gap-3">
-            {pages.map(({ route, title }) =>
-              confirmUserAccess(title) ? (
-                <Link key={title} href={route}>
-                  <li className={getTabClassName(title)}>{title}</li>
-                </Link>
-              ) : (
-                publicPages.includes(title) && (
-                  <Link key={title} href={route}>
-                    <li className={getTabClassName(title)}>{title}</li>
-                  </Link>
-                )
-              )
-            )}
-          </ul>
-        )}
-      </nav>
+        {toggleFlag && <NavigationTabs />}
+      </div>
+      <div className="hidden sm:block">
+        <NavigationTabs />
+      </div>
     </header>
   );
 }
